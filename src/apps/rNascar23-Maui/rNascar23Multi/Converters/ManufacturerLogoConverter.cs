3e293e9de14d282dc5a4ef11f355f7eccd247e75
Rob Roberts <rroberts@centeredgesoftware.com>
2023-05-08 15:24:19 -0400

    Added manufacturer logo to leaderboard rows

---
+++ src/apps/rNascar23-Maui/rNascar23Multi/Converters/ManufacturerLogoConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace rNascar23Multi.Converters
+{
+    internal class ManufacturerLogoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            switch (value.ToString())
+            {
+                case "Chv":
+                    return ImageSource.FromFile("chevrolet.png");
+                case "Frd":
+                    return ImageSource.FromFile("ford.png");
+                case "Tyt":
+                    return ImageSource.FromFile("toyota.png");
+                default:
+                    return ImageSource.FromFile("rnascar_logo_small.png");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
